716c191fb8e618f6569a6e10a60cb5bf388a5cf2
Grant McKenzie <grantdmckenzie@gmail.com>
2015-01-18 15:50:07 -0800

    pull down instagram photos

--- js/map.js
+++ js/map.js
@@ -21,7 +21,9 @@ var histlayers = {};
 
 for(var propt in histimgnames) {
   histlayers[propt] = L.tileLayer('https://tiles.arcgis.com/tiles/4TXrdeWh0RyCqPgB/arcgis/rest/services/'+histimgnames[propt]+'/MapServer/tile/{z}/{y}/{x}', {
-	  attribution: 'UCSB Dept. Geography'
+	  attribution: 'UCSB Dept. Geography',
+	  unloadInvisibleTiles: true, 
+	  maxZoom:18
   });
 }
 
@@ -37,6 +39,36 @@ $('.histlayer').on('click',function() {
 	map.addLayer(histlayers[x[1]]);
     $(this).toggleClass('selected');
     
+});
+
+var _INSTAGRAM =  [];
+var  inst_layer = null;
+
+var monthNames = [ "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" ];
+
+var greenIcon = L.icon({
+    iconUrl: 'img/instagram.png',
+    iconSize:     [24, 24], // size of the icon
+    iconAnchor:   [12, 12], // point of the icon which will correspond to marker's location
+    popupAnchor:  [0, -6] // point from which the popup should open relative to the iconAnchor
+});
+  
+$.ajax({
+   url: 'handlers/instagram.php',
+   error: function(a, b, c) {
+      alert('error');
+   },
+   dataType: 'json',
+   success: function(data) {
+      for(var i=0;i<data.length;i++) {
+	  var da = new Date(data[i].ts*1000);
+	  var df = da.getMonth()+1 +"/"+da.getDate()+"/"+da.getFullYear() + " " + da.getHours() + ":" + (da.getMinutes()<10?'0':'') + da.getMinutes();
+	  _INSTAGRAM.push(L.marker([data[i].lat, data[i].lng],{icon: greenIcon}).bindPopup("<div style='text-align:center;width:200px;'><img src='"+unescape(data[i].thumb)+"'/>" + "<br/>" + data[i].caption+"<br/>"+df+"<br/><img src='img/instagram2.png' style='width:16px;margin-top:5px'/><a href='"+data[i].photo+"' target='_blank'>Full Photo</a></div>"));
+      }
+      inst_layer = L.layerGroup(_INSTAGRAM);
+      
+   },
+   type: 'GET'
 });
 
 function popHistoric() {
@@ -55,5 +87,18 @@ function toggleHistory() {
     } else {
       $('#lhistoric').slideUp();
       $('#tHistory').css("background-image","url('img/rightarrow.png')");
+    }
+}
+
+function toggleSocial() {
+  
+    if ($('#lsocial').css('display') == "none") {
+      $('#lsocial').slideDown();
+      $('#tSocial').css("background-image","url('img/downarrow.png')");
+      inst_layer.addTo(map); 
+    } else {
+      $('#lsocial').slideUp();
+      $('#tSocial').css("background-image","url('img/rightarrow.png')");
+      map.removeLayer(inst_layer);
     }
 }
